9d56294e7daad0c8fe7be687ba43cf21e69cea18
lucien <lucien.edel@geops.de>
2019-02-27 16:32:45 +0100

    Add i18next and configuration, but commented import to avoid its use by default

--- packages/es/index.js
+++ packages/es/index.js
@@ -2,6 +2,7 @@
 import 'core-js';
 import 'react-app-polyfill/ie11';
 import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
+// import './i18n';
 import App from './App';
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './i18n';
 import AppRouter from './AppRouter';
 // import * as serviceWorker from './serviceWorker';
 
