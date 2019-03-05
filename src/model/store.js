58c2753dd611eef6e76cb0e10ec7695aa6f16f8f
lucien <lucien.edel@geops.de>
2019-03-05 10:18:02 +0100

    Use redux-thunk in store

--- src/model/store.js
+++ src/model/store.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export default createStore(reducers);
+// Allow to use Redux dev tools in FF and Chrome
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
